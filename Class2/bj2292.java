c5cda815c61c81b987a4ef1803fd678d9e42c5a9
choiish98 <dnjsqls9119@gmail.com>
2021-01-18 17:07:00 +0900

    Update bj2292.java

--- Class2/bj2292.java
+++ Class2/bj2292.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
 
-public class bj2292 {
+class bj2292 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int sum = 1;
+        int n = 1;
 
-        Scanner in = new Scanner(System.in);
-
-        int N = in.nextInt();
-        int count = 1; // 겹 수(최소 루트)
-        int range = 2; // 범위 (최솟값 기준)
-
-        if (N == 1) {
-            System.out.print(1);
+        while (a > sum) {
+            sum += 6 * n;
+            n++;
         }
 
-        else {
-            while (range <= N) { // 범위가 N보다 커지기 직전까지 반복
-                range = range + (6 * count); // 다음 범위의 최솟값으로 초기화
-                count++; // count 1 증가
-            }
-            System.out.print(count);
-        }
+        System.out.println(n);
     }
 }
