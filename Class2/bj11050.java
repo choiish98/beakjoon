692cf96b96034d17b4baf0e0b415f616a057c93f
choiish98 <dnjsqls9119@gmail.com>
2021-01-19 17:47:03 +0900

    Update bj11050.java

--- Class2/bj11050.java
+++ Class2/bj11050.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class bj11050 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
