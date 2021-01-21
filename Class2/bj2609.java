c5552e45b785b87d119fbecd9a8a881cff145dcd
choiish98 <dnjsqls9119@gmail.com>
2021-01-21 15:15:41 +0900

    Update bj2609.java

--- Class2/bj2609.java
+++ Class2/bj2609.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
-//최대공약수와 최소공배수
-public class bj2609 {
-    static int gcd(int a, int b) {
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
+class bj2609 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(gcf(num1, num2) + " " + lcf(num1, num2));
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int gcd = gcd(a, b);
-        int lcm = (a * b / gcd); // 최소 공배수 구하기
-        System.out.println(gcd);
-        System.out.println(lcm);
-
+    public static int gcf(int num1, int num2) {
+        if (num1 % num2 == 0)
+            return num2;
+        if (num2 % num1 == 0)
+            return num1;
+        for (int i = num1 / 2; i > 0; i--) {
+            if (num1 % i == 0 && num2 % i == 0)
+                return i;
+        }
+        return 0;
     }
 
+    public static int lcf(int num1, int num2) {
+        int i = 1;
+        while (true) {
+            if ((num1 * i) % num2 == 0)
+                break;
+            i++;
+        }
+        return num1 * i;
+    }
 }
