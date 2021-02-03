9738855a6d2fb1c2ce9e9b88b63259556913e6c4
choiish98 <56579239+choiish98@users.noreply.github.com>
2021-02-03 23:23:47 +0900

    Create bj2164.java

---
+++ Class2/bj2164.java
@@ -0,0 +1,20 @@
+public class bj2164 {
+	public static Queue<Integer> queue = new LinkedList<>();
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(queue.size() == 1) {
+				System.out.println(queue.poll());
+				break;
+			}
+			queue.poll();
+			queue.add(queue.poll());
+		}
+	}
+}
