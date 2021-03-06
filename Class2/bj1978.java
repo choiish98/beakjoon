48a625cd3ef479475afe9a70f1ef5907be080705
choiish98 <dnjsqls9119@gmail.com>
2021-03-06 19:07:36 +0900

    Update bj1978.java

--- Class2/bj1978.java
+++ Class2/bj1978.java
@@ -1,35 +1,32 @@
 import java.util.Scanner;
- 
+import java.util.Arrays;
+
 public class bj1978 {
 	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
 		int count = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			// 소수인경우 true, 아닌경우 false   
+
+		for (int i = 0; i < N; i++) {
 			boolean isPrime = true;
-			
-			int num = in.nextInt();
-			
-			if(num == 1) {	// 1 인경우 다음 반복문으로
+			int num = sc.nextInt();
+
+			if (num == 1)
 				continue;
-			}
-			for(int j = 2; j <= Math.sqrt(num); j++) {
-				if(num % j == 0) {
-					isPrime = false;	// 소수가 아니므로 false 로 바꿔줌
+
+			for (int j = 2; j <= Math.sqrt(num); j++) {
+				if (num % j == 0) {
+					isPrime = false;
 					break;
 				}
 			}
-			if(isPrime) {	// 소수인경우 count 값 1 증가
+
+			if (isPrime)
 				count++;
-			}
 		}
+
 		System.out.println(count);
 	}
- 
 }
