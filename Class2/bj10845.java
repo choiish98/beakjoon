2f934e66332f032ec41d73980112b1549d23f019
choiish98 <dnjsqls9119@gmail.com>
2021-03-09 22:50:43 +0900

    Update bj10845.java

--- Class2/bj10845.java
+++ Class2/bj10845.java
@@ -1,87 +1,51 @@
-import java.util.Scanner;
-
-class Queue {
-    private int[] queue;
-    private int frontIndex = 0;
-    private int backIndex = -1;
-
-    Queue() {
-
-    }
-
-    Queue(int number) {
-        queue = new int[number];
-    }
-
-    public void push(int x) {
-        queue[++backIndex] = x;
-    }
-
-    public int size() {
-        return backIndex - frontIndex + 1;
-    }
-
-    public boolean isEmpty() {
-        if (size() == 0)
-            return true;
-        else
-            return false;
-    }
-
-    public int pop() {
-        if (isEmpty())
-            return -1;
-        else
-            return queue[frontIndex++];
-    }
-
-    public int front() {
-        if (isEmpty())
-            return -1;
-        else
-            return queue[frontIndex];
-    }
-
-    public int back() {
-        if (isEmpty())
-            return -1;
-        else
-            return queue[backIndex];
-    }
-
-}
+import java.io.*;
+import java.util.*;
 
 public class bj10845 {
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        Queue queue = new Queue(number);
-        for (int i = 0; i < number; i++) {
-            String command = scanner.next();
-            switch (command) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 명령의 수
+        List<Integer> que = new ArrayList<>(); // 큐 리스트
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String temp = st.nextToken(); // 명령어
+            switch (temp) {
             case "push":
-                int x = scanner.nextInt();
-                queue.push(x);
+                que.add(Integer.parseInt(st.nextToken()));
                 break;
             case "pop":
-                System.out.println(queue.pop());
-                break;
-            case "empty":
-                if (queue.isEmpty())
-                    System.out.println(1);
-                else
-                    System.out.println(0);
-                break;
-            case "front":
-                System.out.println(queue.front());
-                break;
-            case "back":
-                System.out.println(queue.back());
+                if (!que.isEmpty()) {
+                    System.out.println(que.get(0));
+                    que.remove(0);
+                } else {
+                    System.out.println(-1);
+                }
                 break;
             case "size":
-                System.out.println(queue.size());
+                System.out.println(que.size());
                 break;
+            case "empty":
+                if (que.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+                break;
+            case "front":
+                if (que.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(que.get(0));
+                }
+                break;
+            case "back":
+                if (que.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(que.get(que.size() - 1));
+                }
             }
         }
+
     }
 }
