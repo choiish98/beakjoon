13dacf4e9f839ede9f23ae78cb367e270862e159
choiish98 <dnjsqls9119@gmail.com>
2021-03-25 18:56:41 +0900

    Create quickSort.java

---
+++ Algorithm/quickSort.java
@@ -0,0 +1,16 @@
+// 퀵정렬
+// 아무 숫자를 기준으로 잡고
+// 기준 숫자보다 작은 수는 왼쪽으로 파티션
+// 큰 수는 오른쪽으로 파티션
+// 파티션 안에서도 다시 파티션
+// 시간 복잡도 O(n log n) 최악의 경우: n^2
+
+/**
+ * quickSort
+ */
+public class quickSort {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
