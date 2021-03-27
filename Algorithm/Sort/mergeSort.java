1d59b5399c2b5df002a5dc9db433614d2701fe6e
choiish98 <dnjsqls9119@gmail.com>
2021-03-28 02:48:34 +0900

    Create mergeSort.java

---
+++ Algorithm/Sort/mergeSort.java
@@ -0,0 +1,62 @@
+/*  MergeSort
+    정렬되지 않은 리스트를 각각 하나의 원소만 포함하는 n개의 부분리스트로 분할
+    부분리스트가 하나만 남을 때까지 반복해서 병합하며 정렬된 부분리스트를 생성
+    2-way 합병 정렬
+    리스트의 길이가 1 이하이면 이미 정렬된 것으로 본다. 그렇지 않은 경우에는
+    분할(divide) : 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눈다.
+    정복(conquer) : 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
+    결합(combine) : 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다. 이때 정렬 결과가 임시배열에 저장된다.
+    복사(copy) : 임시 배열에 저장된 결과를 원래 배열에 복사한다.
+    O(n): O(n log n)
+*/
+public class mergeSort {
+    private static void mergeSort(int[] arr) {
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1)
+    }
+
+    private static void mergeSort(int[] arr, int tmp[], int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(arr, tmp, start, mid);
+            mergeSort(arr, tmp, mid + 1, end);
+            merge(arr, tmp, start, mid, end);
+        }
+    }
+
+    private static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
+        for (int i = start; i <= end; i++) {
+            tmp[i] = arr[i];
+        }
+        int part1 = start;
+        int part2 = mid + 1;
+        int index = start;
+        while (part1 <= mid && part2 <= end) {
+            if (tmp[part1] <= tmp[part2]) {
+                arr[index] = tmp[part1];
+                part1++;
+            } else {
+                arr[index] = tmp[part2];
+                part2++;
+            }
+            index++;
+        }
+        for (int i = 0; i <= mid - part1; i++) {
+            arr[index + i] = tmp[part1 + i];
+        }
+    }
+
+    private static void PrintArray(int[] arr) {
+        for (int data : arr) {
+            System.out.print(data + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 9, 4, 7, 5, 0, 1, 6, 8, 2 };
+        PrintArray(arr);
+        mergeSort(arr);
+        PrintArray(arr);
+    }
+}
