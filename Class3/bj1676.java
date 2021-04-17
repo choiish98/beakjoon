631728820f0799ae0e0a70286c30616cdd977039
choiish98 <dnjsqls9119@gmail.com>
2021-04-18 05:17:45 +0900

    Update bj1676.java

--- Class3/bj1676.java
+++ Class3/bj1676.java
@@ -3,9 +3,6 @@ import java.util.*;
 
 public class bj1676 {
     public static void main(String[] args) throws IOException {
-        // 수행시간 체크
-        long beforetime = System.currentTimeMillis();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
         int count = 0;
@@ -15,10 +12,5 @@ public class bj1676 {
             num /= 5;
         }
         System.out.println(count);
-
-        // 수행시간 체크
-        long aftertime = System.currentTimeMillis();
-        long secDifftime = (aftertime - beforetime) / 1000;
-        System.out.println("수행시간(m): " + secDifftime);
     }
 }
