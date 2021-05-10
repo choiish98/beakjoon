5ca0cf87e3c6ccd0626cf9fd53d6ff2b33bf342b
choiish98 <dnjsqls9119@gmail.com>
2021-05-10 13:06:16 +0900

    Create anUnfinishedRunner.java

---
+++ programmers/anUnfinishedRunner.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class anUnfinishedRunner {
+    public static void main(String[] args) throws IOException {
+        String[] participant = {};
+        String[] completion = {};
+
+        Solution solution = new Solution(participant, completion);
+
+        System.out.println(solution);
+    }
+}
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        int a = 0;
+
+        for (String pt : participant)
+            map.put(pt, map.getOrDefault(pt, 0) + 1);
+        for (String cp : completion)
+            map.put(cp, map.get(cp) - 1);
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0)
+                answer = key;
+        }
+        return answer;
+    }
+}
